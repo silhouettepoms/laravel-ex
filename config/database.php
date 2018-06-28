c774509bb38d94ec3cefe30e64351383af55df04
silhouettepoms <silhouettepomeranians@gmail.com>
2018-06-28 16:12:11 -0400

    Update database.php

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             //'host' => env(strtoupper(env('DB_SERVICE_NAME', 'MYSQL')).'_SERVICE_HOST', env('DB_HOST', 'localhost')),
-            'host' => 'blahblahblah',
+            'host' => 'dev-ocp-mysql.cluster.net',
             'port' => env(strtoupper(env('DB_SERVICE_NAME', 'MYSQL')).'_SERVICE_PORT', env('DB_PORT', '3306')),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
