0739cd7cedbe71b44baf47137db61f72199bb12a
silhouettepoms <silhouettepomeranians@gmail.com>
2018-07-02 10:29:45 -0400

    Update database.php
    hard coding db connection

--- config/database.php
+++ config/database.php
@@ -54,11 +54,17 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env(strtoupper(env('DB_SERVICE_NAME', 'MYSQL')).'_SERVICE_HOST', env('DB_HOST', 'localhost')),
-            'port' => env(strtoupper(env('DB_SERVICE_NAME', 'MYSQL')).'_SERVICE_PORT', env('DB_PORT', '3306')),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            //'host' => env(strtoupper(env('DB_SERVICE_NAME', 'MYSQL')).'_SERVICE_HOST', env('DB_HOST', 'localhost')),
+           // 'port' => env(strtoupper(env('DB_SERVICE_NAME', 'MYSQL')).'_SERVICE_PORT', env('DB_PORT', '3306')),
+        //    'database' => env('DB_DATABASE', 'forge'),
+         //   'username' => env('DB_USERNAME', 'forge'),
+         //   'password' => env('DB_PASSWORD', ''),
+            'host' => 'dev-ocp-mysql.cluster.net',
+            'database' => 'ceres_iwg_dev',
+            'username' => 'mlinsinb',
+            'password' =>'c$7pxUQt',
+            'port' => '3306',
+         
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
